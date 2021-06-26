17cdccc88fdcff762ec189ee21868ed66ef7e0ad
unknown <hoangnkgcs190118@fpt.edu.vn>
2021-06-26 18:23:25 +0700

    Create_AS2Project

---
+++ AS2project/AS2project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AS2project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
